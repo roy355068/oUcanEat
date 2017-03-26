535875e62eeb7d7100adcbc43236fba3ba22da51
cyc520427 <cyc520427@gmail.com>
2017-03-26 14:49:48 -0400

    setup first view

--- OUCanEat/urls.py
+++ OUCanEat/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from OUCanEat import views
+from . import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
-	url(r'^OUCanEat/', include('OUCanEat.urls')),
 ]
--- OUCanEat/views.py
+++ OUCanEat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	context = {}
+	return render(request, 'OUCanEat/google_sample.html', context)
