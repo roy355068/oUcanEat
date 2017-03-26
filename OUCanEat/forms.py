5a356ae4d482d54bce4af3b058297d48f3b858cb
roy355068 <bochengl@andrew.cmu.edu>
2017-03-26 16:04:17 -0400

    Setup registration and login page

---
+++ OUCanEat/forms.py
@@ -0,0 +1,78 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+from django.db.models.fields.files import FieldFile
+MAX_UPLOAD_SIZE = 2500000
+
+class RegistrationForm(forms.Form):
+	first_name = forms.CharField(max_length = 30)
+	last_name  = forms.CharField(max_length = 30)
+	username   = forms.CharField(max_length = 30)
+	email      = forms.CharField(max_length = 50, 
+							     widget = forms.EmailInput())
+	password1  = forms.CharField(max_length = 150,
+								 label = 'Password',
+								 widget = forms.PasswordInput())
+	password2  = forms.CharField(max_length = 150,
+								 label = 'Confirm password',
+								 widget = forms.PasswordInput())
+	age 	   = forms.IntegerField(min_value = 0)
+	bio		   = forms.CharField(max_length = 430,
+							     widget = forms.Textarea)
+
+	def clean(self):
+
+		cleaned_data = super(RegistrationForm, self).clean()
+		password1 = cleaned_data.get('password1')
+		password2 = cleaned_data.get('password2')
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError("Passwords did not match.")
+		return cleaned_data
+
+	def clean_username(self):
+		username = self.cleaned_data.get('username')
+		if User.objects.filter(username__exact = username):
+			raise forms.ValidationError("Username is already taken.")
+		return username
+
+	
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		exclude = (
+			'user',
+			'created_at'
+		)
+class NameForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = (
+			'first_name',
+			'last_name',
+			)
+
+class EditForm(forms.ModelForm):
+	picture = forms.FileField(required=False, widget=forms.FileInput)
+	age = forms.IntegerField(min_value = 0)
+	class Meta:
+		model = Profile
+		exclude = (
+			'user',
+			'content_type',
+			)
+
+
+	def clean_picture(self):
+		picture = self.cleaned_data['picture']
+		# if not picture:
+		# 		raise forms.ValidationError('You must upload a picture')
+		if not isinstance(picture, FieldFile):
+			if not picture:
+				raise forms.ValidationError("You must upload a picture")
+			if not picture.content_type or not picture.content_type.startswith('image'):
+				raise forms.ValidationError('File type is not image')
+			if picture.size > MAX_UPLOAD_SIZE:
+				raise forms.ValidationError('File too big (max size is {0} bytes)'.format(MAX_UPLOAD_SIZE))
+		return picture
+	
--- OUCanEat/models.py
+++ OUCanEat/models.py
@@ -1,5 +1,28 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Post(models.Model):
+	content    = models.TextField(max_length = 160)
+	user 	   = models.ForeignKey(User, default = None)
+	created_at = models.DateTimeField(auto_now_add = True)
+
+	def __unicode__(self):
+		return "id: " + self.id + " " + self.user + "create at " + self.created_at
+		
+class Profile(models.Model):
+	user 		 = models.ForeignKey(User, default = None)
+	age 		 = models.IntegerField()
+	bio 		 = models.TextField(max_length = 430, blank = True)
+	picture 	 = models.FileField(upload_to = "images", blank = True)
+	content_type = models.CharField(max_length = 50, blank = True)
+
+class Follow(models.Model):
+	following = models.ForeignKey(User, default = None, related_name = "following") 
+	follower  = models.ForeignKey(User, default = None, related_name = "follower")
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, default = None)
+	content = models.TextField(max_length = 160)
+	created_at = models.DateTimeField(auto_now_add = True)
+	commented_on = models.ForeignKey(Post, default = None)
