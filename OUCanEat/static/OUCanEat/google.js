c539a5067d4a9a7178278aaf9062a150a2722c54
cyc520427 <cyc520427@gmail.com>
2017-04-18 23:34:00 -0400

    clean html template

--- OUCanEat/static/OUCanEat/google.js
+++ OUCanEat/static/OUCanEat/google.js
@@ -81,14 +81,12 @@ function profileMap() {
 		dataType: "json",
 		success: function(response) {
 			var restaurants = JSON.parse(response.restaurants);
-			console.log(restaurants);
 			showMapEvents(restaurants, true, false);
 		}
 	})
 }
 
 function showMapEvents(event_restaurants, clear, fromSearch) {
-	console.log(event_restaurants);
 	var color = fromSearch ? 'green': 'purple';
 	var bounds = map.getBounds();
 	if (bounds===undefined || clear) {
@@ -100,7 +98,6 @@ function showMapEvents(event_restaurants, clear, fromSearch) {
 	var google_ids = new Set();
 
 	$(event_restaurants).each(function(value) {
-		console.log(value);
 		if (google_ids.has(this.fields.google_id)) return;
 		service.getDetails({placeId: this.fields.google_id}, function (result, status) {
 			if (status==google.maps.places.PlacesServiceStatus.OK) {
